e068617617429eb66fc8bd3244695d3dd8627a66
Juan Cahuana <jcahuanam@gmail.com>
2020-04-08 13:38:45 -0500

    Add bootstrap, jquery and popper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,4 @@
+// Punto de entrada principal de'
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -8,6 +9,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// Agrega bootstrap al proyecto
+import 'bootstrap'
+import 'css/styles'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
