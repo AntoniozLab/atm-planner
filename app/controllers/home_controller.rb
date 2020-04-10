355d1434e4a266ba5b58efa4c39765772eb3c2c5
Juan Cahuana <jcahuanam@gmail.com>
2020-04-10 16:42:59 -0500

    Add example to verify login status

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
+# Controlador de la págiona de bienvenida
 class HomeController < ApplicationController
+
+  # Verifica si esta logueado (para pruebas)
+  # before_action :authenticate_user!
+
   def index
   end
 end
