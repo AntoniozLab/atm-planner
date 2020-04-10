77aa1554c507ded6c4e10b927fc750b941f808da
Juan Cahuana <jcahuanam@gmail.com>
2020-04-10 16:45:58 -0500

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_010446) do
+ActiveRecord::Schema.define(version: 2020_04_10_212239) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
