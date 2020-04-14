6fb4dfc1a483a58c8422c6bd9e38dac90175a17d
Juan Cahuana <jcahuanam@gmail.com>
2020-04-14 18:24:19 -0500

    Add force table delete if exists other connections

--- db/migrate/20200412032451_drop_table_requerimientos.rb
+++ db/migrate/20200412032451_drop_table_requerimientos.rb
@@ -1,5 +1,6 @@
 class DropTableRequerimientos < ActiveRecord::Migration[6.0]
   def change
-    drop_table :requerimientos
+    # drop_table :requerimientos
+    drop_table(:requerimientos, force: true) if ActiveRecord::Base.connection.tables.include?('requerimientos')
   end
 end
