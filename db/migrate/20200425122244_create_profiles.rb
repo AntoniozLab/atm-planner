fb9446c34fec7a76a682cad4af34c23cad1aa6e0
Juan Cahuana <jcahuanam@gmail.com>
2020-04-25 09:48:16 -0500

    Fix type name
    Cambia String por string todo en minúscula

--- db/migrate/20200425122244_create_profiles.rb
+++ db/migrate/20200425122244_create_profiles.rb
@@ -1,9 +1,9 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-      t.String :name
-      t.String :type
-      t.String :description
+      t.string :name
+      t.string :type
+      t.string :description
     end
   end
 end
