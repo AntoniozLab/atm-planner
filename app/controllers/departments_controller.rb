976e9feb14e206facd6374f914f8e6676fc2698d
Vladimir Jaco Berrospi <Vladimir.Jaco@etexgroup.com>
2020-04-26 20:22:47 -0500

    update views

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -4,7 +4,7 @@ class DepartmentsController < ApplicationController
   # GET /departments
   # GET /departments.json
   def index
-    @departments = Department.all
+    @departments = Department.all.order(:name)
   end
 
   # GET /departments/1
