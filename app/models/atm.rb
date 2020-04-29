8d10136c2b55db793145eb4d4cceb5fbbfd083cf
AlonsoVento <alonso_vento@outlook.com>
2020-04-29 11:38:15 -0500

    Retoques finales a las Vistas de ATM / se realizó validaciones en las Vistas de ATM

--- app/models/atm.rb
+++ app/models/atm.rb
@@ -3,6 +3,7 @@ class Atm < ApplicationRecord
   belongs_to :atm_type
   belongs_to :atm_state
 
- 
+  validates :serial_number, uniqueness: { message: ": Está repetido"}
+
 
 end
--- app/models/atm_brand.rb
+++ app/models/atm_brand.rb
@@ -1,2 +1,4 @@
 class AtmBrand < ApplicationRecord
+    validates :name, presence: { message: "Ingrese el valor"}
+    validates :name, uniqueness: { message: "Marca ya existe"}
 end
