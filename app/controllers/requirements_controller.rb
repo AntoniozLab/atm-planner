b26bb318a8a6648a07529d04ed58433d9a6c3e28
Juan Cahuana <jcahuanam@gmail.com>
2020-06-13 23:37:33 -0500

    Fix states validation from details to header requirement

--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -46,11 +46,56 @@ class RequirementsController < ApplicationController
     end
   end
 
+  def update_atm_state(rqTypeId, atmOldId, atmNewId)
+    puts '-------- update_atm_state ------------'
+    # puts "rqTypeId: " + rqTypeId
+    puts "atmOldId: " + atmOldId.to_s
+    puts "atmNewId: " + atmNewId.to_s
+
+    # Cambio de estado para nueva instalación completada, pasa a produccion (id 1)
+    if rqTypeId.to_s === "1"
+      puts '=========> Pasa a produccción'
+      Atm.find(atmNewId).update_attributes(:atm_state_id => 1)
+    end
+
+    # Cambio de estado para retiro completo, pasa a stock (id 2)
+    if rqTypeId.to_s === "2"
+      puts '===============> Pasa a Stock'
+
+      Atm.find(atmOldId).update_attributes(:atm_state_id => 2)
+    end
+
+    # Cambio de estado para movimiento completado, el nuevo pasa a produccion(id 1) y el anterior pasa a stock (id 2)
+    if rqTypeId.to_s === "3"
+      puts 'tressssssssssssssss'
+
+      Atm.find(atmNewId).update_attributes(:atm_state_id => 1)
+      Atm.find(atmOldId).update_attributes(:atm_state_id => 2)
+    end
+
+
+  end
+
   # PATCH/PUT /requirements/1
   # PATCH/PUT /requirements/1.json
   def update
+
     respond_to do |format|
       if @requirement.update(requirement_params)
+        puts '--------------- update -------------------'
+        puts requirement_params
+        detailsObject = requirement_params[:requirement_details_attributes]["0"]
+
+        # Verifica si existe end_date de la operación del cajero
+        if detailsObject.has_key? "end_date(1i)" and detailsObject["end_date(1i)"] != ""
+
+          completionDate = Date.new(detailsObject["end_date(1i)"].to_i, detailsObject["end_date(2i)"].to_i, detailsObject["end_date(3i)"].to_i)
+          rqTypeId = detailsObject[:requirement_type_id]
+          atmNewId = detailsObject[:atm_new]
+          atmOldId = detailsObject[:atm_old]
+          update_atm_state(rqTypeId, atmOldId, atmNewId)
+        end
+
         format.html { redirect_to @requirement, notice: 'Requirement was successfully updated.' }
         format.json { render :show, status: :ok, location: @requirement }
       else
@@ -79,6 +124,27 @@ class RequirementsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def requirement_params
       # params.require(:requirement).permit(:description, :request_date, :completion_date, :percentage_success, :user_assigned, :attachment_url, :user_id, :requirement_state_id, :location_id)
+
+
+      # Recuperado al fecha de completado del cajero
+      detailsObject = params[:requirement][:requirement_details_attributes]["0"];
+
+      # Asigna una fecha de completado al requerimiento usando la fecha de completado del detalle de requerimiento
+
+      # Verifica que existe una fecha de completado
+      if detailsObject.has_key? "end_date(1i)" and detailsObject["end_date(1i)"] != ""
+        completionDate = Date.new(detailsObject["end_date(1i)"].to_i, detailsObject["end_date(2i)"].to_i, detailsObject["end_date(3i)"].to_i)
+
+        # Asigna la fecha de completado a todo el requerimiento
+        params[:requirement][:completion_date] = completionDate
+
+        # Asigna estado "completado" al requerimiento
+        params[:requirement][:requirement_state_id] = 2
+
+        # Cambia el estado a completado del detalle de requerimiento
+        puts "================================ ok ================"
+        puts params[:requirement][:requirement_details_attributes]["0"][:requirement_detail_state_id] = 3
+      end
 
       # Se le agrega al final todos los parámetros de un detalle de requerimiento
       params.require(:requirement).permit(:description, :request_date, :completion_date, :percentage_success, :user_assigned, :attachment_url, :user_id, :requirement_state_id, :location_id, requirement_details_attributes: RequirementDetail.attribute_names.map(&:to_sym).push(:_destroy))
